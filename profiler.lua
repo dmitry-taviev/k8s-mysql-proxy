f7003d2b3369fa1a9d59e48aae17391fab3e18b5
Dmitry Taviev <dmitry.taviev@applyit.lv>
2016-10-24 17:14:48 +0300

    formatting output

--- profiler.lua
+++ profiler.lua
@@ -1,11 +1,11 @@
 function read_query(packet)
 	if packet:byte() == proxy.COM_QUERY then
-		print(os.date() .. " query: " .. packet:sub(2))
+		-- print(os.date() .. " query: " .. packet:sub(2))
 		proxy.queries:append(1, packet, {resultset_is_needed = false})
 		return proxy.PROXY_SEND_QUERY
 	end
 end
 
 function read_query_result(inj)
-	print(os.date() .. " query-time: " .. (inj.query_time / 1000) .. "ms, " .. "response-time: " .. (inj.response_time / 1000) .. "ms")
+	print("[" .. os.date() .. "] " .. inj.query .. " [query-time=" .. (inj.query_time / 1000) .. "ms, " .. "response-time=" .. (inj.response_time / 1000) .. "ms]")
 end
