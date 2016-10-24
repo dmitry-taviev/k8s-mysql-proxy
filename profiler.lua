bffe413a48e112ac2380a050d84399b9a0dd739c
Dmitry Taviev <dmitry.taviev@applyit.lv>
2016-10-24 16:56:52 +0300

    adding timers

--- profiler.lua
+++ profiler.lua
@@ -1,12 +1,11 @@
 function read_query(packet)
 	if packet:byte() == proxy.COM_QUERY then
 		print(os.date() .. " query: " .. packet:sub(2))
-		-- proxy.queries:append(1, packet )
-		-- return proxy.PROXY_SEND_QUERY
+		proxy.queries:append(1, packet, {resultset_is_needed = false})
+		return proxy.PROXY_SEND_QUERY
 	end
 end
 
--- function read_query_result(inj)
--- 	print("query-time: " .. (inj.query_time / 1000) .. "ms")
--- 	print("response-time: " .. (inj.response_time / 1000) .. "ms")
--- end
+function read_query_result(inj)
+	print(os.date() .. " query-time: " .. (inj.query_time / 1000) .. "ms, " .. "response-time: " .. (inj.response_time / 1000) .. "ms")
+end
